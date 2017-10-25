6e631586c1b7fbb3e1e845493a428c48ab725ff7
Johnatonb <john.brubaker.1@mypisd.net>
2017-10-25 19:36:35 +0000

    added firebase

---
+++ Scripts.js
@@ -0,0 +1,8 @@
+//3~WYOUJCb1FHOX6AQLoyqexXZFNswsmsML5INirPUCLYHlt5zCBuwLujxwszKeRIYv
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "AIzaSyCop2X7UxLYubaxVwevOWNkHuwLNVzZdfM",
+  authDomain: "productive-4382e.firebaseapp.com",
+};
+firebase.initializeApp(config);
